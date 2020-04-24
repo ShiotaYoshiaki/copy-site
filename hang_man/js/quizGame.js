87d31f731fc4845a05aa893c0d046c76c2c79505
tanaka-yuu <tanaka07yuu23@gmail.com>
2020-04-24 18:11:50 +0900

    add a few function 202004241811

--- hang_man/js/quizGame.js
+++ hang_man/js/quizGame.js
@@ -3,20 +3,30 @@
 import { serveOneQuiz } from './question.js';
 const ANSWER = document.getElementById('answer');
 const HINT_LIST = document.getElementById('hintList');
+const MISTAKE_LIST = document.getElementById('mistakeList');
 const INPUT_BTN = document.getElementById('inputBtn');
 const INPUT_ANSWER = document.getElementById('inputAnswer')
+const HANG_MAN = document.getElementById('hangMan');
+const HANG_MANS_SRC = new Array("../images/h2.png","../images/h3.png","../images/h4.png","../images/h5.png","../images/h6.png","../images/h7.png","../images/h8.png","../images/h9.png")
 let questionCount = 0;
-let mistakeCount = 0;
+let mistakeCount;
+let correctCount;
 let CURRENT_QUIZ;
+let answerCopy;
+
 
 export function runningGame (){
+  correctCount = 0;
+  mistakeCount = 0;
   CURRENT_QUIZ = serveOneQuiz(questionCount);
   setQuestion();
   setHint(mistakeCount);
+  addHangMan(mistakeCount);
 }
 
 function setQuestion() {//問題をセットする
-  ANSWER.textContent = "_".repeat(CURRENT_QUIZ.a.length);
+  answerCopy = "_".repeat(CURRENT_QUIZ.a.length);
+  ANSWER.textContent = answerCopy;
 }
 
 function setHint(i) {//ヒントを追加する
@@ -25,19 +35,43 @@ function setHint(i) {//ヒントを追加する
   HINT_LIST.appendChild(NEW_HINT);
 }
 
-function setAnswer(i,char){
-  ANSWER.textContent = re
+function setMistakeList(char) {//ヒントを追加する
+  const NEW_MIS = document.createElement('li');
+  NEW_MIS.textContent = char;
+  MISTAKE_LIST.appendChild(NEW_MIS);
+}
+
+function setAnswer(i,char){//i番目の文字を表示する
+  answerCopy = answerCopy.substr(0,i) + char + answerCopy.substr(i+1,answerCopy.length-i);
+  ANSWER.textContent = answerCopy;
+}
+
+function addHangMan(i){
+  HANG_MAN.src=HANG_MANS_SRC[i];
 }
 
 function judgment(char){
   var correct = false;
   for(let i=0;i<CURRENT_QUIZ.a.length;i++){
     if(CURRENT_QUIZ.a.charAt(i)===char){
-
+      correctCount++;
+      setAnswer(i,char);
+      correct=true;
+    }else{
     }
+  }
+  if(correct){
+    
+  }else{
+    mistakeCount++;
+    setHint(mistakeCount);
+    setMistakeList(char);
+    addHangMan();
   }
 }
 
 INPUT_BTN.addEventListener('click', () => {
-  
+  judgment(INPUT_ANSWER.value);
+  INPUT_ANSWER.value = '';
+  INPUT_ANSWER.focus();
 });
