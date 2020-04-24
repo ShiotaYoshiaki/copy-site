1c08c7f898f3f4b6df74fc73f86a08031e508dd1
tanaka-yuu <tanaka07yuu23@gmail.com>
2020-04-24 15:31:29 +0900

    add a few function in quizGame.js

--- hang_man/js/quizGame.js
+++ hang_man/js/quizGame.js
@@ -2,15 +2,42 @@
 
 import { serveOneQuiz } from './question.js';
 const ANSWER = document.getElementById('answer');
+const HINT_LIST = document.getElementById('hintList');
+const INPUT_BTN = document.getElementById('inputBtn');
+const INPUT_ANSWER = document.getElementById('inputAnswer')
 let questionCount = 0;
+let mistakeCount = 0;
 let CURRENT_QUIZ;
 
 export function runningGame (){
   CURRENT_QUIZ = serveOneQuiz(questionCount);
   setQuestion();
+  setHint(mistakeCount);
 }
 
-
-function setQuestion() {
+function setQuestion() {//問題をセットする
   ANSWER.textContent = "_".repeat(CURRENT_QUIZ.a.length);
 }
+
+function setHint(i) {//ヒントを追加する
+  const NEW_HINT = document.createElement('li');
+  NEW_HINT.textContent = CURRENT_QUIZ.h[i];
+  HINT_LIST.appendChild(NEW_HINT);
+}
+
+function setAnswer(i,char){
+  ANSWER.textContent = re
+}
+
+function judgment(char){
+  var correct = false;
+  for(let i=0;i<CURRENT_QUIZ.a.length;i++){
+    if(CURRENT_QUIZ.a.charAt(i)===char){
+
+    }
+  }
+}
+
+INPUT_BTN.addEventListener('click', () => {
+  
+});
